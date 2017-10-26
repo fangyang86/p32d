c4880c2060aab93c33685b7187ca2b1b77f2fce2
gbm618 <gbm618@gmail.com>
2017-10-26 17:16:22 +0800

    init bat

--- bat0.bat
+++ bat0.bat
@@ -1,3 +1,9 @@
-ls
-ls
+rm -f psi.PO
+cp psi.PO.000 psi.PO
+rm -f pcubemx/MDK-ARM/pcubemx.uvopt
+rm -f pcubemx/MDK-ARM/pcubemx.uvproj
+cp pcubemx/MDK-ARM/pcubemx.uvopt.000 pcubemx/MDK-ARM/pcubemx.uvopt
+cp pcubemx/MDK-ARM/pcubemx.uvproj.000 pcubemx/MDK-ARM/pcubemx.uvproj
+ll psi.PO psi.PO.000
+ll pcubemx/MDK-ARM
 

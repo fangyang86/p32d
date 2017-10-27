72740fa82d6f9087e6cac99bd43e43d5c908ec89
gbm618 <gbm618@gmail.com>
2017-10-27 16:53:29 +0800

    add git.ignore bat.pre

---
+++ batpre.bat
@@ -0,0 +1,15 @@
+rm -f psi.PO.000
+rm -f psi.PO.001
+cp psi.PO psi.PO.000
+cp psi.PO psi.PO.001
+rm -f pcubemx/MDK-ARM/pcubemx.uvopt.000
+rm -f pcubemx/MDK-ARM/pcubemx.uvproj.000
+rm -f pcubemx/MDK-ARM/pcubemx.uvopt.001
+rm -f pcubemx/MDK-ARM/pcubemx.uvproj.001
+cp pcubemx/MDK-ARM/pcubemx.uvopt pcubemx/MDK-ARM/pcubemx.uvopt.000
+cp pcubemx/MDK-ARM/pcubemx.uvproj pcubemx/MDK-ARM/pcubemx.uvproj.000
+cp pcubemx/MDK-ARM/pcubemx.uvopt pcubemx/MDK-ARM/pcubemx.uvopt.001
+cp pcubemx/MDK-ARM/pcubemx.uvproj pcubemx/MDK-ARM/pcubemx.uvproj.001
+ll psi.PO psi.PO.00?
+ll pcubemx/MDK-ARM
+
